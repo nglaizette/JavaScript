c75b660bf3ff12340fa876b96747afd709e7f563
NicoG <winnipeg.francois@gmail.com>
2024-04-07 19:02:34 +0200

    Ajout d'un bouton pour rajouter des points au hasard sans superposition

--- js/maths/graph.js
+++ js/maths/graph.js
@@ -9,6 +9,18 @@ class Graph {
 		this.points.push(point);
 	}
 
+	containsPoint(point) {
+		return this.points.find((p) => p.equals(point));
+	}
+
+	tryAddPoint(point) {
+		if(!this.containsPoint(point)){
+			this.addPoint(point);
+			return true;
+		}
+		return false;
+	}
+
 	draw(ctx) {
 		for (const segment of this.segments) {
 			segment.draw(ctx);
