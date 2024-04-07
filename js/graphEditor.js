dc33b51f82fae3abddc20a565faff561ad4d8ec5
NicoG <winnipeg.francois@gmail.com>
2024-04-07 23:11:35 +0200

    Affichage du dernier point créé d'une manière différente des autres

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -4,6 +4,8 @@ class GraphEditor{
 		this.graph = graph;
 
 		this.ctx = this.canvas.getContext("2d");
+
+		this.selected = null;
 		
 		this.#addEventListener();
 	}
@@ -12,10 +14,14 @@ class GraphEditor{
 		this.canvas.addEventListener("mousedown", (evt) => {
 			const mousePoint = new Point(evt.offsetX, evt.offsetY);
 			this.graph.addPoint(mousePoint);
+			this.selected = mousePoint;
 		});
 	}
 
 	display() {
 		this.graph.draw(this.ctx);
+		if(this.selected) {
+			this.selected.draw(this.ctx, {outline: true});
+		}
 	}
 }
