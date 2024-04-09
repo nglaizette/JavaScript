23e27dc1c9cad0e4065271cb5b8e259f51cb03f0
NicoG <winnipeg.francois@gmail.com>
2024-04-09 21:49:42 +0200

    Ajout du calcul du zoom

--- js/viewport.js
+++ js/viewport.js
@@ -13,6 +13,10 @@ class Viewport {
 	}
 
 	#handleMouseWheel(event) {
-		console.log(event.deltaY)
+		const dir = Math.sign(event.deltaY);
+		const step = 0.1;
+		this.zoom += dir * step;
+		this.zoom = Math.max(1, Math.min(5, this.zoom));
+		console.log(this.zoom)
 	}
 }
