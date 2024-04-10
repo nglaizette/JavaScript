566558055ecc9333cfff682de87db3fe4943a60d
NicoG <winnipeg.francois@gmail.com>
2024-04-10 23:24:28 +0200

    Création du monde et des enveloppes pour chaque segment!

---
+++ js/world.js
@@ -0,0 +1,26 @@
+class World {
+	constructor(graph, roadWith = 100, roadRoundness = 3){
+		this.graph = graph;
+		this.roadWith = roadWith;
+		this.roadRoundness = roadRoundness;
+
+		this.envelopes = [];
+
+		this.generate();
+	}
+
+	generate() {
+		this.envelopes.length = 0;
+		for(const segment of this.graph.segments) {
+			this.envelopes.push(
+				new Envelope(segment, this.roadWith, this.roadRoundness)
+			);
+		}
+	}
+
+	draw(ctx) {
+		for (const envelope of this.envelopes){
+			envelope.draw(ctx);
+		}
+	}
+}
