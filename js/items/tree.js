24daa63b03dc531654d02f1bc5e5f6c307f43afd
NicoG <winnipeg.francois@gmail.com>
2024-04-13 21:58:19 +0200

    Gestion de la hauteur des arbrea en fonction de la distance au centre de l'image.

--- js/items/tree.js
+++ js/items/tree.js
@@ -1,14 +1,15 @@
 class Tree {
-	constructor(center, size){
+	constructor(center, size, heightCoefficient = 0.3){
 		this.center = center;
 		this.size = size; // size of the base
+		this.heightCoefficient = heightCoefficient
 	}
 
 	draw(ctx, viewPoint) {
 		const difference = substract(this.center, viewPoint);
 		this.center.draw(ctx, {size: this.size, color: "green"});
 
-		const top = add(this.center, {x: -40, y: -40});
+		const top = add(this.center, scale(difference, this.heightCoefficient));
 		new Segment(this.center, top).draw(ctx);
 	}
 }
