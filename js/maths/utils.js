6a2e2a5e553f54af1a1a02a8256982312178928a
NicoG <winnipeg.francois@gmail.com>
2024-04-13 16:48:55 +0200

    Correction bug approximation de s intersection à epsilon près

--- js/maths/utils.js
+++ js/maths/utils.js
@@ -54,10 +54,10 @@ function angle(point){
 function getIntersection(A, B, C, D){
 	const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
 	const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
-	const bottom = (D.y -C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
+	const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
 
-	const epsilon = 0.001
-	if ( Math.abs(bottom-epsilon) > 0 ){
+	const epsilon = 0.001;
+	if ( Math.abs(bottom) > epsilon ){
 		const t = tTop / bottom;
 		const u = uTop / bottom;
 		if( t >= 0 && t <= 1 && u >= 0 && u <= 1){
