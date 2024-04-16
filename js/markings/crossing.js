4489bd0591c0577c0a51fb030ec5ebed7ce76257
NicoG <winnipeg.francois@gmail.com>
2024-04-16 23:20:01 +0200

    Refactoring des "marking" Stop et Crossing

--- js/markings/crossing.js
+++ js/markings/crossing.js
@@ -1,16 +1,6 @@
-class Crossing {
+class Crossing extends Marking{
 	constructor(center, directionVector, width, height){
-		this.center = center;
-		this.directionVector = directionVector;
-		this.width = width;
-		this.height = height;
-
-		this.support = new Segment(
-			translate(center, angle(directionVector), height / 2),
-			translate(center, angle(directionVector), -height / 2)
-		)
-
-		this.polygon = new Envelope(this.support, width, 0).polygon;
+		super(center, directionVector, width, height);
 		this.borders = [
 			this.polygon.segments[0],
 			this.polygon.segments[2]
